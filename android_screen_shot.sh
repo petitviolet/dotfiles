b75f808fab5609ad1183d2d22b689452be6ce8d0
petitviolet <violethero0820@gmail.com>
2014-10-30 17:10:16 +0900

    androidのスクリーンショット撮るスクリプト消した

--- android_screen_shot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-DATE_TIME=`date +"%Y%m%d-%H%M%S"`
-FILE_NAME=${DATE_TIME}.png
-
-adb shell screencap -p /sdcard/${FILE_NAME}
-adb pull /sdcard/${FILE_NAME}
-adb shell rm /sdcard/${FILE_NAME}
-convert -resize x480 ${FILE_NAME} ${FILE_NAME}
