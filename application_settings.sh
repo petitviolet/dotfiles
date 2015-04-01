eda388f805fbe8bb0308a880d06a0a0d9d27b9fa
petitviolet <violethero0820@gmail.com>
2015-04-02 08:20:05 +0900

    add application settings

---
+++ application_settings.sh
@@ -0,0 +1,3 @@
+ln -sf ~/dotfiles/Preferences/Karabiner/private.xml /Users/petitviolet/Library/Application\ Support/Karabiner/private.xml
+ln -sf ~/dotfiles/Preferences/IntelliJ/com.jetbrains.intellij.ce.plist ~/Library/Preferences/
+ln -sf ~/dotfiles/Preferences/AndroidStudio/settings.jar ~/Library/Preferences/AndroidStudio/settings.jar
