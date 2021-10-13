fd06bbde70794c8157d9ba0367ae624107f358ae
petitviolet <violethero0820@gmail.com>
2021-10-13 22:09:37 +0900

    nvim.sh

--- nvim.sh
+++ nvim.sh
@@ -3,14 +3,14 @@
 mkdir -p $HOME/.config
 ln -s $(dirname $PWD)/$(basename $PWD)/nvim $HOME/.config/nvim
 
-pyenv install 2.7.18
-pyenv virtualenv 2.7.18 neovim-py2
-pyenv shell 2.7.18
-pip install pynvim
+pyenv install 2.7.18 || true
+pyenv virtualenv 2.7.18 neovim-py2 || true
+pyenv shell 2.7.18 || true
+pip install pynvim || true
 
-pyenv install 3.7.4
-pyenv virtualenv 3.7.4 neovim-py3
-pyenv shell 3.7.4
-pip install pynvim
+pyenv install 3.7.4 || true
+pyenv virtualenv 3.7.4 neovim-py3 || true
+pyenv shell 3.7.4 || true
+pip install pynvim || true
 
-brew install nvim
+brew install nvim || true
