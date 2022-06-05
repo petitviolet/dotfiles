38b9aca3deed0d8dc21ef9f12d57516fb332e47d
petitviolet <violethero0820@gmail.com>
2022-06-05 23:36:52 +0900

    updated Python versions for nvim

--- nvim.sh
+++ nvim.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
-mkdir -p $HOME/.config
-ln -s $(dirname $PWD)/$(basename $PWD)/nvim $HOME/.config/nvim
+mkdir -p $HOME/.config || true
+ln -s $(dirname $PWD)/$(basename $PWD)/nvim $HOME/.config/nvim || true
 
 pyenv install 2.7.18 || true
 pyenv virtualenv 2.7.18 neovim-py2 || true
 pyenv shell 2.7.18 || true
 pip install pynvim || true
 
-pyenv install 3.7.4 || true
-pyenv virtualenv 3.7.4 neovim-py3 || true
-pyenv shell 3.7.4 || true
+pyenv install 3.10.2 || true
+pyenv virtualenv 3.10.2 neovim-py3 || true
+pyenv shell 3.10.2 || true
 pip install pynvim || true
 
 brew install nvim || true
